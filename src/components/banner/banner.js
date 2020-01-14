b19e908fed6e5a8636884718998e06b1299885ef
Lyubov <aira_dark@live.ru>
2020-01-14 18:54:10 +0300

    end

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
 import NavMenu from '../navMenu/navMenu';
 
+
+
 export default class Banner extends Component {
 
   render() {
     const {urlBgImg, text} = this.props;
+
+    const imgStyle = {
+      background: `url(${urlBgImg})`,
+      backgroundSize: '100%'
+    }
     return (
       <>
-        <div className="banner" style={{background: `url(${urlBgImg})`}}>
+        <div className="banner" style={imgStyle}>
           <div className="container">
             <div className="row">
               <div className="col-lg-6">
